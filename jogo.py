670f2011c30abaa7f8956dd1ce3efdaf99746fcd
narasz <150243565+narasz@users.noreply.github.com>
2025-02-21 22:26:51 -0300

    jogo

---
+++ jogo.py
@@ -0,0 +1,15 @@
+print("**************")
+print("Bem Vindo ao Jogo de Adivinhação!")
+print("**************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número:")
+print ("Você digitou:", chute_str)
+
+
+if (numero_secreto == chute):
+     print ("Você acertou!")
+else:
+    print("Você errou!" )
+print ("Fim do jogo")
